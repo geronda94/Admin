69afb18c5f3a40aafa9add3606fde077e6c63f75
geronda94 <iiiliev.igor@gmail.com>
2024-10-14 20:00:01 +0300

    124141

---
+++ admin/src/main.js
@@ -0,0 +1,69 @@
+import { createApp } from 'vue'
+import './style.css'
+import './assets/style/reset.css'
+import App from './App.vue'
+
+
+import router from './router'
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHeart, faHand, faAddressBook, faCreditCard, faLemon, faHourglass } from '@fortawesome/free-regular-svg-icons'
+library.add([faHeart, faHand, faAddressBook, faCreditCard, faLemon, faHourglass])
+
+
+// import PrimeVue from "primevue/config";
+// import Lara from '@primevue/themes/lara';
+// import { definePreset } from '@primevue/themes'
+
+
+const app = createApp(App)
+
+
+
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+
+// const MyPreset = definePreset(Lara, {
+//   semantic: {
+//     primary: {
+//         50: '{cyan.50}',
+//         100: '{cyan.100}',
+//         200: '{cyan.200}',
+//         300: '{cyan.300}',
+//         400: '{cyan.400}',
+//         500: '{cyan.500}',
+//         600: '{cyan.600}',
+//         700: '{cyan.700}',
+//         800: '{cyan.800}',
+//         900: '{cyan.900}',
+//         950: '{cyan.950}'
+//         }
+//       }
+//     }
+// );
+
+
+// app.use(PrimeVue, {
+//     theme: {
+//       preset: MyPreset,
+//     },
+//     options: {
+//       prefix: 'p',
+//       darkModeSelector: 'system',
+//       cssLayer: false
+//   }
+
+//   });
+
+
+
+
+
+
+app.use(router)
+app.mount('#app')
+
+
